ec3c38d4fff925032d997877827f1714e48a116c
adairconlin <adair.conlin@gmail.com>
2022-01-10 15:35:19 -0500

    Add Handlebars for MVC use

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+const path = require("path");
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
+//set up Handlebars.js
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //turn on routes
 app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
+//set up Handlebars.js
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 
 //turn on connection to db server
 //`force: true` will make tables re-create if there are any association changes
